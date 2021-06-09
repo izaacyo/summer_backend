0536add030b79198c96a305204999d2cf575fed2
Samu <77488879+sase93@users.noreply.github.com>
2021-06-09 15:43:16 +0300

    Mainly code cleaning.

--- server.js
+++ server.js
@@ -1,34 +1,24 @@
-require("dotenv/config"); // See the connection below for info!
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const mongoose = require("mongoose");
 const bp = require('body-parser');
 const { connect } = require("mongoose");
-
-const { DB, PORT } = require("./config");
-const { success, error } = require('consola')
-
-const passport = require('passport')
-
-
-// This connects to the database. The URL etc. is hidden by using a .env file - refer to: https://www.youtube.com/watch?v=uk-iRSj1Ldg ~27minute mark.
-//mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true });
-
+const { DB, PORT } = require("./config"); // Hides database connection info using config/index.js and .env files.
+const { success, error } = require('consola');
+const passport = require('passport');
 
 //Middlewares
-app.use(bp.json())
+app.use(bp.json());
 app.use(cors());
 app.use(express.json());
-app.use(passport.initialize())
-
+app.use(passport.initialize());
 
 require("./middlewares/Passport")(passport);
 
-// Allowing files saved by multer to be accessed.
-app.use(express.static('public')); // You need to create a public folder for static assets (images), refer to: https://stackoverflow.com/questions/34984347/url-to-file-which-was-uploaded-with-multer
+// Allowing files saved by multer to be accessed, refer to: https://stackoverflow.com/questions/34984347/url-to-file-which-was-uploaded-with-multer
+app.use(express.static('public'));
 
-app.use("/", require("./routes/routes")); // Apparently sets the route paths to be used.
+app.use("/", require("./routes/routes")); // Sets the route paths to be used [instead of doing app.use("/add", ..) which is also possible.]
 
 // User router Middlewares
 
